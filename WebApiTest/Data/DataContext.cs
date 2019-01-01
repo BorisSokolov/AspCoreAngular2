3296cfb5dafc15b38de5e104e38df8ddca55bc1a
Boris Sokolov <boris.sokolov@valtech.nl>
2019-01-01 22:50:37 +0200

    Updating DataContext

--- WebApiTest/Data/DataContext.cs
+++ WebApiTest/Data/DataContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApiTest.Data
 {
@@ -13,5 +9,6 @@ namespace WebApiTest.Data
         }
 
         public DbSet<Models.Value> Values { get; set; }
+        public DbSet<Models.User> Users { get; set; }
     }
 }
