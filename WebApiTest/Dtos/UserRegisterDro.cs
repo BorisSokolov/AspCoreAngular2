0ba81386e0029ffa3a9dac44302de05eebc09dd0
Boris Sokolov <boris.sokolov@valtech.nl>
2019-01-06 17:58:03 +0200

    Adding validation

--- WebApiTest/Dtos/UserRegisterDro.cs
+++ WebApiTest/Dtos/UserRegisterDro.cs
@@ -1,8 +1,15 @@
-﻿namespace WebApiTest.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiTest.Dtos
 {
     public class UserRegisterDro
     {
+        [Required]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "User name should have at least 1 and not more than 30 chars")]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 4, ErrorMessage = "Password should have at least 4 and not more than 30 chars")]
         public string Password { get; set; }
     }
 }
