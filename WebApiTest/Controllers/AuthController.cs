4230eee9fc471993ecb88da2b6a5eebedf57d13e
Boris Sokolov <boris.sokolov@valtech.nl>
2019-01-09 16:25:29 +0200

    Adding login action

--- WebApiTest/Controllers/AuthController.cs
+++ WebApiTest/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using WebApiTest.Data;
 using WebApiTest.Dtos;
 using WebApiTest.Models;
@@ -11,14 +17,16 @@ namespace WebApiTest.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _configuration;
 
-        public AuthController(IAuthRepository authRepository)
+        public AuthController(IAuthRepository authRepository, IConfiguration configuration)
         {
             _authRepository = authRepository;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody]UserRegisterDro userRegisterDro)
+        public async Task<IActionResult> Register(UserRegisterDro userRegisterDro)
         {
             userRegisterDro.UserName = userRegisterDro.UserName.ToLower();
 
@@ -35,6 +43,43 @@ namespace WebApiTest.Controllers
             var createdUser = await _authRepository.Register(newUser, userRegisterDro.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var user = await _authRepository.Login(userLoginDto.UserName.ToLower(), userLoginDto.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
---
+++ WebApiTest/Dtos/UserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiTest.Dtos
+{
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
