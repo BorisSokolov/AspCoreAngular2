c417990d805bc8a780e8595853a0c231acab294a
Boris Sokolov <boris.sokolov@valtech.nl>
2019-01-11 17:38:25 +0200

    Adding menu bar

---
+++ WebApiTest/DatingAppSPA/src/app/nav/nav.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
+})
+/** nav component*/
+export class NavComponent {
+    /** nav ctor */
+    constructor() {
+
+    }
+}
